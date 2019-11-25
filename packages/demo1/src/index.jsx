7c11a5d3abf82c184fafbf448d5b355812a6b3a4
lvchunai <lvchunai@fangdd.com>
2019-11-25 17:58:39 +0800

    init

---
+++ packages/demo1/src/index.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './app';
+ReactDOM.render(<div>This is demo1</div>, document.getElementById('root'));
---
+++ packages/demo2/webpack.config.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack');
+
+module.exports = () => {
+  return {
+    entry: [],
+    output: {},
+    resolve: {
+      extensions: [],
+    },
+    module: [],
+    plugin: [],
+  };
+}
