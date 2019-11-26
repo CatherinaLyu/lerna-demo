d126b7840be5fa1494cb47225c7723e022e017a2
lvchunai <lvchunai@fangdd.com>
2019-11-26 11:32:16 +0800

    independent

--- packages/demo2/src/index.jsx
+++ packages/demo2/src/index.jsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './app';
-ReactDOM.render(<div>This is demo2</div>, document.getElementById('root'));
+ReactDOM.render(<div>This is demo2, and  now we are independent mode</div>, document.getElementById('root'));
