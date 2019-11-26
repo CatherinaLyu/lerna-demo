5d8a73f87627f4220d3b39321654f291a798d706
lvchunai <lvchunai@fangdd.com>
2019-11-26 11:40:28 +0800

    test

--- packages/demo1/src/index.jsx
+++ packages/demo1/src/index.jsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './app';
-ReactDOM.render(<div>This is demo1</div>, document.getElementById('root'));
+ReactDOM.render(<div>This is demo1, I just want to publish</div>, document.getElementById('root'));
